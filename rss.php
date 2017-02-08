4356b78b30a9f0364c67dc3404efa61f79c989c1
Nick Vellios <nick@vellios.com>
2017-02-08 09:51:32 -0800

    Edits

--- rss.php
+++ rss.php
@@ -1,13 +1,10 @@
 <?PHP
 header('Content-Type: application/xml');
 
-$feed = file_get_contents('http://feeds.washingtonpost.com/rss/rss_volokh-conspiracy');
-$xml = new SimpleXMLElement($feed);
+$xml = new SimpleXMLElement(file_get_contents('http://feeds.washingtonpost.com/rss/rss_volokh-conspiracy'));
 
 foreach ($xml->channel->item as $item) {
-	$dc = $item->children('http://purl.org/dc/elements/1.1/');
-	$creator = $dc->creator;
-	$item->title = '[' . $creator . '] ' . $item->title;
+	$item->title = '[' . $item->children('http://purl.org/dc/elements/1.1/')->creator . '] ' . $item->title;
 }
 
 echo $xml->asXML();
